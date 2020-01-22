26a94ebbbbf2a030437791cbb1843b1b2ddf2072
Rhys Evans <r.evans@qmul.ac.uk>
2020-01-22 12:13:45 +0000

    Code coverage test (CoDiMa)

--- gap/srgs.gd
+++
@@ -1,9 +0,0 @@
-#
-# srgs: A library of small strongly regular graphs and related functions
-#
-# Declarations
-#
-
-#! @Description
-#!   Insert documentation for you function here
-DeclareGlobalFunction( "srgs_Example" );
